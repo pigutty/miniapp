86b957f49abd8fced5c43a61236d1cf4d77d204d
pigutty <50047826+pigutty@users.noreply.github.com>
2019-05-04 23:43:08 +0900

    create message function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,17 +1,28 @@
 class MessagesController < ApplicationController
   def index
+    @messages = Message.includes(:user).page(params[:page]).per(5).order("created_at DESC")
   end
 
   def show
+    @message = Message.find(params[:id])
   end
 
   def new
+    @user = User.find(params[:user_id])
+    @message = Message.new
   end
 
   def create
+    Message.create(message_params)
+    redirect_to controller: :messages, action: :index
   end
 
   def destroy
+    message = Message.find(params[:id])
+    if message.user_id == current_user.id
+      message.destroy
+    end
+    redirect_to root_path
   end
 
   def edit
@@ -19,6 +30,10 @@ class MessagesController < ApplicationController
 
   def update
   end
-  
+
+  private
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
 
 end
