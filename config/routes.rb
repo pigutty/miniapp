6bbe37df5f0533ab1af7935dccfd671763af8901
pigutty <50047826+pigutty@users.noreply.github.com>
2019-05-04 19:50:49 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
-  resources :user, only[:show, :edit, :update] do
+  resources :user, only: [:show, :edit, :update] do
     resources :messages, only: [:index,:create,:show]
   end
+  root "messages#index"
 end
