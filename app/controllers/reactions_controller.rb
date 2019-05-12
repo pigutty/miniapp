ddb37f3d59260ab621bcce2803125e949e204af9
pigutty <50047826+pigutty@users.noreply.github.com>
2019-05-13 01:58:29 +0900

    stamp function
    It is not possible to operate the reaction model from message model view

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -1,13 +1,22 @@
 class ReactionsController < ApplicationController
+  before_action :set_user
+  def new
+    @reaction = Reaction.new
+  end
+
   def create
-    @user = User.find(params[:user_id])
-    @message = Message.find(params[:message_id])
-    Reaction.create(reaction_params)
+    reaction = Reaction.create(reaction_params)
   end
 
   private
 
   def reaction_params
     params.require(:reaction).permit(:stamp_type, :stamp_number).merge(message_id: params[:message_id], user_id: current_user.id)
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
+    @message = Message.find(params[:message_id])
+    @message_id = @message.id
   end
 end
