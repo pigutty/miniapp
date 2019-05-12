023df62c805794249be9da320797537d85e012ec
pigutty <50047826+pigutty@users.noreply.github.com>
2019-05-13 00:09:24 +0900

    Update 20190504154613_create_reactions.rb

--- db/migrate/20190504154613_create_reactions.rb
+++ db/migrate/20190504154613_create_reactions.rb
@@ -3,6 +3,8 @@ class CreateReactions < ActiveRecord::Migration[5.2]
     create_table :reactions do |t|
       t.string :stamp_type
       t.integer :stamp_number
+      t.references :user, foreign_key: true
+      t.references :message, foreign_key: true
       t.timestamps
     end
   end
